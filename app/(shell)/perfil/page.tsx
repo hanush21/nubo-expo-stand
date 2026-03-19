1740c417f7cab939c3413f54b83a7bca8253436a
hanush <112359676+hanush21@users.noreply.github.com>
2026-03-19 11:07:18 +0000

    Initial commit

---
+++ app/(shell)/perfil/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1>Página de perfil</h1>
+            <p>Contenido del perfil va aquí.</p>
+            <p>Este es un ejemplo de contenido del perfil.</p>
+        </div>
+    )
+}
+
+export default page
