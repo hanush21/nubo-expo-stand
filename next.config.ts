b23bdcf4ada43c30097d9dab2853b9a094085394
Hanush <112359676+hanush21@users.noreply.github.com>
2026-03-23 10:43:52 +0000

    legal terms

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.CI ? "export" : undefined,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
